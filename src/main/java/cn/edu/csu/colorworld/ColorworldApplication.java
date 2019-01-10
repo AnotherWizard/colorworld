f25f7073f1cc45105d591173d4316813835252d1
RoyIronGrey <15665512229@163.com>
2019-01-10 13:35:50 +0800

    "test lyz 1-10 13:35"

--- src/main/java/cn/edu/csu/colorworld/ColorworldApplication.java
+++ src/main/java/cn/edu/csu/colorworld/ColorworldApplication.java
@@ -2,10 +2,17 @@ package cn.edu.csu.colorworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ColorworldApplication {
 
+    @RequestMapping("/")
+    String home() {
+        return "<b>Hello World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ColorworldApplication.class, args);
     }
